3ba9b32880b151e04d8566d9a4fb1408a535edcb
Winnaries <w.winnaries@gmail.com>
2019-12-14 23:51:50 +0700

    Freshed People App

---
+++ people/app/database.js
@@ -0,0 +1,12 @@
+
+const db_ip = process.env.DATABASE_IP || 'localhost';
+const db_port = process.env.DATABASE_PORT || '27017';
+const db_user = process.env.DATABASE_USER || 'lemon';
+const db_pass = process.env.DATABASE_PASS || '12345';
+const db_auth = process.env.DATABASE_AUTH || 'admin';
+
+const db_url = `mongodb://${db_user}:${db_pass}@${db_ip}:${db_port}/${db_auth}`;
+
+module.exports.db_url = db_url;
+
+
